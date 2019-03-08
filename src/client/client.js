23231aa890582af2a7d0905a199e05fce1782aac
Sathya <svakacharla@sapient.com>
2019-03-08 18:20:54 +0530

    map reducers to store

--- src/client/client.js
+++ src/client/client.js
@@ -6,6 +6,7 @@ import Routes from './Routes';
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import {Provider} from 'react-redux';
+import reducers from './reducers';
 
 const store = createStore(reducers, {}, applyMiddleware(thunk))
 
